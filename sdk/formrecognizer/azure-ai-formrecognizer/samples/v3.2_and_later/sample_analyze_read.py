47ba2f49ad339353dad055967bb08fc300649d1d
v-ruihanshan <v-ruihanshan@microsoft.com>
2023-09-19 16:35:34 +0800

    add sample_analyze_read

--- sdk/formrecognizer/azure-ai-formrecognizer/samples/v3.2_and_later/sample_analyze_read.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/samples/v3.2_and_later/sample_analyze_read.py
@@ -60,9 +60,7 @@ def analyze_read():
     )
     with open(path_to_sample_documents, "rb") as f:
         poller = document_analysis_client.begin_analyze_document(
-            "prebuilt-read", document=f,
-            features=
-            [AnalysisFeature.STYLE_FONT,
+            "prebuilt-read", document=f, features=[AnalysisFeature.STYLE_FONT,
              AnalysisFeature.FORMULAS,
              AnalysisFeature.BARCODES,
              AnalysisFeature.OCR_HIGH_RESOLUTION,
